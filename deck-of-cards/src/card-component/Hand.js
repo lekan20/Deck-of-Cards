36780c1dc8363dadef8e72d84fad8b2caa642eca
Lekan <lekan.nicholson@gmail.com>
2019-09-18 14:21:30 -0400

    forgot to save before pushing

--- deck-of-cards/src/card-component/Hand.js
+++ deck-of-cards/src/card-component/Hand.js
@@ -10,6 +10,7 @@ class Hand extends React.Component {
 
   render() {
     return (
+      // Create for loop to print out five version of the cards
       <div>
       <Card deckId={this.props.deckId} />
       <Card deckId={this.props.deckId} />
